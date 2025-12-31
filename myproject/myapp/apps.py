5f2899d766d402781c2e69706361cae59d04418c
christianbusinness <christianbusinness@gmail.com>
2025-12-31 12:22:17 +0800

    new

--- myproject/myapp/apps.py
+++ myproject/myapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class OnlinecourseConfig(AppConfig):
+class MyappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'onlinecourse'
+    name = 'myapp'
